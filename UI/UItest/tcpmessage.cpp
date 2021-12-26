125b56ca88bc2840aab36e9c685420824fceb65d
Vorrad <15217340356@163.com>
2021-12-26 14:18:02 +0800

    New ip tcp Classes, gain parameters from page

---
+++ UI/UItest/tcpmessage.cpp
@@ -0,0 +1,6 @@
+#include "tcpmessage.h"
+
+TcpMessage::TcpMessage()
+{
+
+}
---
+++ UI/UItest/tcpmessage.h
@@ -0,0 +1,11 @@
+#ifndef TCPMESSAGE_H
+#define TCPMESSAGE_H
+
+
+class TcpMessage : public IpMessage
+{
+public:
+    TcpMessage();
+};
+
+#endif // TCPMESSAGE_H
