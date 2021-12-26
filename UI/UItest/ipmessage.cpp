711c3b0473cfdfe480eae2d81edeffd38557f2e0
Vorrad <15217340356@163.com>
2021-12-26 11:43:54 +0800

    New class IpMessage

---
+++ UI/UItest/ipmessage.cpp
@@ -0,0 +1,6 @@
+#include "ipmessage.h"
+
+IpMessage::IpMessage()
+{
+
+}
---
+++ UI/UItest/ipmessage.h
@@ -0,0 +1,11 @@
+#ifndef IPMESSAGE_H
+#define IPMESSAGE_H
+
+
+class IpMessage
+{
+public:
+    IpMessage();
+};
+
+#endif // IPMESSAGE_H
